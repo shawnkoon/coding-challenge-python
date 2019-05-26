f1f341985ebd908827016e4042d277b627b3d730
shawnkoon <shawn9405@gmail.com>
2019-05-25 19:21:21 -0700

    Add basic solution & template file for starter

---
+++ src/merge_k_sorted_linked_lists/app.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class MergeKSortedLinkedLists:
+    """
+    Source : https://leetcode.com/problems/merge-k-sorted-lists/
+
+    Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+    Example:
+
+    Input:
+    [
+        1->4->5,
+        1->3->4,
+        2->6
+    ]
+    Output: 1->1->2->3->4->4->5->6
+    """
+
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        tot = []
+        head = cur = ListNode(None)
+
+        for list_item in lists:
+            while list_item:
+                tot.append(list_item.val)
+                list_item = list_item.next
+
+        for i in sorted(tot):
+            cur.next = ListNode(i)
+            cur = cur.next
+
+        return head.next
---
+++ src/merge_k_sorted_linked_lists/test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from .app import MergeKSortedLinkedLists
+
+
+class MergeKSortedLinkedListsTest(unittest.TestCase):
+    def setUp(self):
+        self.tester = MergeKSortedLinkedLists()
+
+    def test_case_1(self):
+        pass
