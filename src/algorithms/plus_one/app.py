a6f7559878061cf3691427eef80c693e6d756f2b
shawnkoon <shawn9405@gmail.com>
2019-06-15 08:33:34 +0900

    Add leetcode problem 66. Plus One

---
+++ src/algorithms/plus_one/app.py
@@ -0,0 +1,43 @@
+from typing import List
+from collections import deque
+
+
+class PlusOne:
+    """
+    Source : https://leetcode.com/problems/plus-one/
+
+    Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+
+    The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+
+    You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+
+    Input: [1,2,3]
+    Output: [1,2,4]
+    Explanation: The array represents the integer 123.
+    Example 2:
+
+    Input: [4,3,2,1]
+    Output: [4,3,2,2]
+    Explanation: The array represents the integer 4321.
+    """
+
+    def function(self, digits: List[int]) -> List[int]:
+        stack = deque()
+
+        pw = 1
+        for n in reversed(digits):
+            num = n + pw
+
+            pw = 1 if num >= 10 else 0
+            stack.appendleft(num % 10)
+
+        if not stack:
+            stack.append(pw)
+        else:
+            if pw:
+                stack.appendleft(pw)
+
+        return list(stack)
