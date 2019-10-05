d508761e9bd7543e92b0f91f630a75c5f3acf944
shawnkoon <shawn9405@gmail.com>
2019-10-04 21:03:24 -0700

    Add leetcode problem 104. Maximum Depth of Binary Tree & Create TreeNode class method

---
+++ src/algorithms/max_depth_of_binary_tree/app.py
@@ -0,0 +1,31 @@
+from ...datastructures.TreeNode import TreeNode
+
+
+class MaxDepthOfBinaryTree:
+    """
+    Source : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+    Given a binary tree, find its maximum depth.
+
+    The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+    Note: A leaf is a node with no children.
+
+    Example:
+
+    Given binary tree [3,9,20,null,null,15,7],
+
+         3
+        / \\
+       9  20
+          / \\
+         15  7
+    return its depth = 3.
+    """
+
+    def max_depth(self, root: TreeNode) -> int:
+        # Base case
+        if not root:
+            return 0
+
+        return 1 + max(self.max_depth(root.left), self.max_depth(root.right))
--- src/datastructures/TreeNode.py
+++ src/datastructures/TreeNode.py
@@ -1,4 +1,29 @@
+from typing import List
+
+
 class TreeNode:
+    @classmethod
+    def from_list(cls, ara: List[int]):
+        if not ara:
+            return None
+
+        def build_tree(index: int):
+            if index >= len(ara):
+                return None
+
+            if ara[index] is None:
+                return None
+
+            root = cls(ara[index])
+
+            root.left = build_tree(index * 2 + 1)
+
+            root.right = build_tree(index * 2 + 2)
+
+            return root
+
+        return build_tree(0)
+
     def __init__(self, value):
         self.left = None
         self.right = None
